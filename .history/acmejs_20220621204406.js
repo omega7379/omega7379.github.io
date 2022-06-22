016d1a9f8f2d5397094a09d1590dd48a209dba3c
Neiro <marveljoeproxy@gmail.com>
2022-06-21 22:46:28 -0700

    missing pages
    changed to external js and added support + corporate lore pages

---
+++ .history/acmejs_20220621204406.js
@@ -0,0 +1,12 @@
+/*enable switch for css pages-->*/
+                    function toggleTheme(){
+                        var theme = document.getElementsByTagName('link')[0];
+
+                        if (theme.getAttribute('href') == 'acmeStyleGuideDark.css'){
+                            theme.setAttribute('href', 'acmeStyleGuideLight.css');
+                        }
+                        else {
+                            theme.setAttribute('href', 'acmeStyleGuideDark.css');
+                        }
+                    }
+                
---
+++ .history/acmejs_20220621204503.js
@@ -0,0 +1,10 @@
+/*enable switch for css pages-->*/
+   function toggleTheme(){
+       var theme = document.getElementsByTagName('link'){}  
+            if (theme.getAttribute('href') == 'acmeStyleGuideDark.css'){
+           theme.setAttribute('href', 'acmeStyleGuideLight.css');
+       }
+       else {
+           theme.setAttribute('href', 'acmeStyleGuideDark.css');
+       }
+   }
